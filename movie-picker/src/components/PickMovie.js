9af878d7b1a724eb84c902580a2a7e7a5d249e50
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-05-18 12:49:37 -0700

    Update PickMovie.js

--- movie-picker/src/components/PickMovie.js
+++ movie-picker/src/components/PickMovie.js
@@ -20,14 +20,14 @@ const PickMovie = () => {
     
     return (
         <div>
-            <h1 className="title">Pick a Movie</h1>
+            <h1 className="title">Add a Receipt</h1>
             {picked && 
             <div className ="picked">
                 <div></div>
                 <Movie name={randomMovie.name} image={randomMovie.img} genre={randomMovie.genre}/>
                 <button className="pickButton" onClick={pickAgain}>Pick Again</button>
             </div>}
-            {!picked && <button onClick={moviePicker}>Pick a Movie</button>}
+            {!picked && <button onClick={moviePicker}>Add a Receipt</button>}
         </div>
     );
 }
