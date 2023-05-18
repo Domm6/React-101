f892c1655f7662241f9bd5e7304239d6ac86730e
Dominic Parker <100439777+Domm6@users.noreply.github.com>
2023-05-18 13:35:51 -0700

    Update Header.js

--- movie-picker/src/components/Header.js
+++ movie-picker/src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
         <div className="header">
             <h1 className="logo"> Movie Picker</h1>
             <div className="links">
-                <Link to="/">Movie Catalog</Link>
                 <Link to="/movie">Upload Receipt</Link>
                 <Link to="/add">Add Movie</Link>
                 <Link to="/login">Login</Link>
